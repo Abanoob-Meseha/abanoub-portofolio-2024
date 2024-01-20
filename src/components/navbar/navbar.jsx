fbaa9fedd6126ffb91a258ac7d3d3e2aa1f4a21d
unknown <abanoob.meseha@gmail.com>
2024-01-20 18:11:35 +0200

    Home page with navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import styles from './navbar.module.scss'
+import { FaHome } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
+import { IoBriefcase } from "react-icons/io5";
+import { IoMdMail } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <div className={styles.nav}>
+        <Link href='/'><span>HOME</span> <FaHome/></Link>
+        <Link href='/'><span>ABOUT</span> <FaUserTie/></Link>
+        <Link href='/'><span>PORTOFOLIO</span> <IoBriefcase/></Link>
+        <Link href='/'><span>CONTACT</span> <IoMdMail/></Link>
+    </div>
+  )
+}
+
+export default Navbar
