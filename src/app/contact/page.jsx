10a4b1076e7e9217d923a09fc40f0626e20a72f8
unknown <abanoob.meseha@gmail.com>
2024-01-21 14:33:04 +0200

    Done:Contact page and changed the darkcyan color with neon one

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,14 @@
 import Link from 'next/link'
 import styles from './contact.module.scss'
+import { IoMdMail } from "react-icons/io";
+import { IoMdCall } from "react-icons/io";
+import { FaFacebookF ,FaTelegramPlane ,FaWhatsapp,FaGithub} from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div>
         <div className={styles.title}>
-            <h1>GET IN TOUCH</h1>
+            <h1>GET IN <span>TOUCH</span></h1>
         </div>
         <div className={styles.container}>
             <div className={styles.info}>
@@ -15,17 +18,23 @@ const Contact = () => {
                 I am always open to discussing new projects, 
                 creative ideas or opportunities to be part of your visions.
                 </p>
-                <h2>Mail Me</h2>
-                <h2>Call Me</h2>
-                <div>
-                    <Link href="/">f</Link>
-                    <Link href="/">f</Link>
-                    <Link href="/">f</Link>
-                    <Link href="/">f</Link>
-
+                <h2><IoMdMail className={styles.icon}/><span>Mail Me</span>abanoob.meseha@gmail.com</h2>
+                <h2><IoMdCall className={styles.icon}/><span>Call Me</span>01275458882</h2>
+                <div className={styles.social}>
+                    <Link href="/"><FaFacebookF /></Link>
+                    <Link href="/"><FaWhatsapp /></Link>
+                    <Link href="/"><FaGithub /></Link>
+                    <Link href="/"><FaTelegramPlane /></Link>
                 </div>
             </div>
             <div className={styles.form}>
+                <div>
+                    <input type="text" name='name' placeholder='Name'/>
+                    <input type="email" name="email" placeholder='Email'/>
+                    <input type="text" name='subject' placeholder='Subject'/>
+                </div>
+                <textarea name="message" cols="40" rows="7" placeholder='Message'></textarea>
+                <Link href={'/'}><span>Send Message</span> <FaTelegramPlane className={styles.icon}/></Link>
 
             </div>
         </div>
